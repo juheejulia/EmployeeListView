4714612ec1237dc4505c0fea484bdd61553da632
Juhee Kang <juhee.kang@infotiv.se>
2023-10-13 01:00:00 +0200

    wip MainActivity.java

--- app/src/main/java/com/example/employeelistview/MainActivity.java
+++ app/src/main/java/com/example/employeelistview/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.employeelistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText firstNameInput, lastNameInput, roleInput, ageInput,
+            emailInput, mobilePhoneInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstNameInput = findViewById(R.id.firstName);
+        lastNameInput = findViewById(R.id.lastName);
+        roleInput = findViewById(R.id.role);
+        ageInput = findViewById(R.id.age);
+        emailInput = findViewById(R.id.email);
+        mobilePhoneInput = findViewById(R.id.mobile_number);
+        passwordInput = findViewById(R.id.password);
     }
 }
