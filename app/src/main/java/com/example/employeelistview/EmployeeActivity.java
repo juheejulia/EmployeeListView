931a3b507c602522ddc12bd64b50212e613ecd32
Juhee Kang <juhee.kang@infotiv.se>
2023-10-13 12:17:08 +0200

    add btn_back function on EmployeeActivity.java

--- app/src/main/java/com/example/employeelistview/EmployeeActivity.java
+++ app/src/main/java/com/example/employeelistview/EmployeeActivity.java
@@ -2,8 +2,11 @@ package com.example.employeelistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 // This Activity is the page that deploy information of the registered employee.
@@ -11,6 +14,7 @@ import android.widget.ListView;
 public class EmployeeActivity extends AppCompatActivity {
 
     ListView listView;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,20 @@ public class EmployeeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_emloyee);
 
         listView = findViewById(R.id.listViewEmployees);
+        backButton = findViewById(R.id.btn_back);
 
-        ArrayAdapter<Employee> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, DataManager.employees);
+        ArrayAdapter<Employee> adapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_list_item_1, DataManager.employees);
         //EmployeeAdapter adapter = new EmployeeAdapter(this, DataManager.employees);
 
         listView.setAdapter(adapter);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EmployeeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
